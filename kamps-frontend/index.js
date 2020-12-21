d517c4c3c480efb4fe1e8e08b16cbdc97366f679
nicmlu <NicoleMLucien@gmail.com>
2020-12-21 07:33:41 -0500

    wrote method to fetch camps from db

--- kamps-frontend/index.js
+++ kamps-frontend/index.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {});
 
-    reviews = []
-    camps = []
+reviews = [];
+camps = [];
 
-    const BASE_URL = "http://127.0.0.1:3000";
+const BASE_URL = "http://127.0.0.1:3000";
 
-    getCamps() {
-        // make a fetch request to /camps
-        fetch(`${this.BASE_URL}/camps`)
-            .then(resp => resp.json())
-            .then(data => console.log(data))
-        // populate the items and menus properties with the returned data
-        // call renderItems
-        .catch(err => alert(err))
-    }
-
-    renderCamps(){
-        // create DOM nodes and insert data into them to render in the DOM
-    }
+function getCamps() {
+  // make a fetch request to /camps
+  fetch(`${this.BASE_URL}/camps`)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    // populate the items and menus properties with the returned data
+    // call renderItems
+    .catch(err => alert(err));
+}
