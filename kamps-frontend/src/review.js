b1a09b57646d66cebc0d0fcfb72a8483e1928201
nicmlu <NicoleMLucien@gmail.com>
2020-12-21 07:18:20 -0500

    created Review class

---
+++ kamps-frontend/src/review.js
@@ -0,0 +1,11 @@
+class Review {
+  static all = [];
+
+  constructor(id, approve, comment, name, camp_id) {
+    this.id = id;
+    this.approve = approve;
+    this.comment = comment;
+    this.name = name;
+    this.camp_id = camp_id;
+  }
+}
