85965c8dfab40883a727da835b4d41a39bb012c2
nicmlu <NicoleMLucien@gmail.com>
2020-12-21 07:37:34 -0500

    added camp class

---
+++ kamps-frontend/src/camp.js
@@ -0,0 +1,23 @@
+class Camp {
+  constructor(
+    id,
+    img_src,
+    name,
+    description,
+    website,
+    borough,
+    zip,
+    phone,
+    street_address
+  ) {
+    this.id = id;
+    this.img_src = img_src;
+    this.name = name;
+    this.description = description;
+    this.website = website;
+    this.borough = borough;
+    this.zip = zip;
+    this.phone = phone;
+    this.street_address = street_address;
+  }
+}
