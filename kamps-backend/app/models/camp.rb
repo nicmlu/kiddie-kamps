233b285910287f5391ca891ab1452f7abb2c5b5a
nicmlu <NicoleMLucien@gmail.com>
2020-12-21 03:00:05 -0500

    created camp and review classes

--- kamps-backend/app/models/camp.rb
+++ kamps-backend/app/models/camp.rb
@@ -1,4 +1,12 @@
 class Camp < ApplicationRecord
     has_many :reviews 
+    validates :img_src, presence: true
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :website, presence: true
+    validates :borough, presence: true
+    validates :zip, presence: true
+    validates :phone, presence: true
+    validates :street_address, presence: true
     
 end
